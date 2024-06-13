0f008d9aedb7713a1113dab86b9e3a80586e6ee8
mackolak <anna.cahojova@gmx.com>
2024-06-13 18:15:55 +0200

    Initial commit

---
+++ src/main/java/system_bug_tracker/SystemBugTrackerApplication.java
@@ -0,0 +1,13 @@
+package system_bug_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBugTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemBugTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/system_bug_tracker/SystemBugTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package system_bug_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBugTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
